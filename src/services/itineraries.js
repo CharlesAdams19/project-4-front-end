8dfb8caeeb45012dfd2b301d3e1b0a3d10eb881c
Charles Adams 19 <adamscra19@gmail.com>
2025-06-11 17:14:30 +0100

    base url change

--- src/services/itineraries.js
+++ src/services/itineraries.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from '../utils/auth'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
 
 export const getAllItineraries = async () => {
   try {
